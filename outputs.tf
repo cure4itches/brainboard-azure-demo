d1170c4e3a21d98d513c06d285e0bb7979387d1e
Brainboard <no-reply@brainboard.co>
2022-04-25 01:45:08 +0000

    Brainboard update

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
 output "ssh-private-key" {
   sensitive = true
-  value     = tls_private_key.demo-ssh-key.private_key_pem
+  value     = tls_private_key.demo_ssh_key.private_key_pem
+}
+
+output "vm-public-ip" {
+  sensitive = false
+  value     = "${azurerm_public_ip.demo-publicip.*.ip_address}"
 }
 
